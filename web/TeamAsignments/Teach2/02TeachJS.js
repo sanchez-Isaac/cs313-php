a0ce2c9632775919e891e45b73786c72c368c306
Isaac Sanchez <san16044@byui.edu>
2019-05-01 21:32:11 -0500

    The team02 files update

---
+++ web/TeamAsignments/Teach2/02TeachJS.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    alert("Clicked!");
+}
+
+function myFunctionTwo() {
+    var x = document.getElementById("colors").value;
+    document.getElementById("div1").style.backgroundColor = x;
+}
