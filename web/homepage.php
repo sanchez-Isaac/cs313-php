5a64e02881e0330495fc734e3e9723d9a018e25c
Isaac Sanchez <san16044@byui.edu>
2019-05-01 21:48:52 -0500

    Prove 2 files

---
+++ web/homepage.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>01 Prove : Assignment - Hello World</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="styleSheet.css" rel="stylesheet" />
+
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><img src="img/byuilogo.png"></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="Prove2.php">Home</a></li>
+                <li><a href="assignments.html">Assignments</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right"></ul>
+        </div>
+    </div>
+</nav>
+<br>
+<div class="container text-center">
+    <h1>Isaac Sanchez</h1>
+</div>
+<br>
+<br>
+<br>
+<br>
+<div class="row">
+    <div id="yo" class="col-sm-5">
+        <br>
+        <br>
+        <img src="img/yo.jpg" id="icon">
+    </div>
+    <div class="col-sm-6">
+        <h2 class="container-fluid bg-2 text-center"> About Me</h2>
+        <h4 class="container-fluid bg-2 text-center">
+            Im a guy that loves computers, sports, music
+            and music concerts, I spend a lot of money
+            on music. <br>  <br> <br> <br>
+
+            The next part is information from wikipedia
+
+
+
+
+        </h4>
+        <br>
+        <h2 class="container-fluid bg-2 text-center">
+            BYU-I at Wikipedia
+        </h2>
+        <h4 class="container-fluid bg-2 text-center">
+            Brigham Young University–Idaho (BYU–Idaho or BYU–I) is a private university located in Rexburg, Idaho. Founded in 1888, the university is owned and operated by The Church of Jesus Christ of Latter-day Saints (LDS Church), transitioned from a junior college to a four-year institution in 2001, and was known for the greater part of its history as Ricks College.
+
+            BYU-Idaho offers programs in liberal arts including the sciences, engineering, agriculture, management, and performing arts. The university is broadly organized into thirty-three departments within six colleges, and its parent organization, the Church Educational System (CES), sponsors sister schools in Utah and Hawaii. The university's focus is on undergraduate education, hosting 26 certificate, 20 associate and over 87 bachelor's degree programs; and it operates using a three-semester system also known as "tracks".
+
+            Students at BYU-Idaho are required to follow an honor code, which mandates behavior in line with LDS teachings (e.g., academic honesty, adherence to dress and grooming standards, and abstinence from extramarital sex and from the consumption of drugs and alcohol). Approximately 99 percent of the university's students are members of the LDS Church, and a significant percentage of the student body take an 18- (women) or 24-month (men) hiatus from their studies to serve as missionaries.[4] A BYU-Idaho education is generally less expensive than similar private universities, due largely to significant funding by LDS Church tithing funds, helping keep tuition rates low.[5]
+        </h4>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="footer">
+    <p>CS 313 - Web Engineering II    &copy; 2016 - <?php echo date("Y");?> </p>
+
+</div>
+
+</body>
+</html>
+
+
