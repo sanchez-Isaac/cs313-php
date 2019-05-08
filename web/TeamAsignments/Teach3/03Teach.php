6e541ac13902fa4c4eebe910cc3a302daca0db4f
Isaac Sanchez <san16044@byui.edu>
2019-05-08 14:32:42 -0500

    Team assignment mod

--- web/TeamAsignments/Teach3/03Teach.php
+++ web/TeamAsignments/Teach3/03Teach.php
@@ -1,10 +1,10 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-mailto: <?php echo $_POST["email"];?><br><br>
-Major: <?php echo $_POST["major"];?><br>
-comments:<?php echo $_POST["comment"];?><br>
+Welcome <?php echo htmlspecialchars($_POST["name"]); ?><br>
+mailto: <?php echo htmlspecialchars($_POST["email"]);?><br><br>
+Major: <?php echo htmlspecialchars($_POST["major"]);?><br>
+comments:<?php echo htmlspecialchars($_POST["comment"]);?><br>
 
 </body>
 </html>
