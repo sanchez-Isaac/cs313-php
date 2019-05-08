e3f18dbbef8984fd382d984b687fd9de7fc03200
Isaac Sanchez <san16044@byui.edu>
2019-05-08 15:11:24 -0500

    Team assignment part 3

--- web/TeamAsignments/Teach3/03Teach.php
+++ web/TeamAsignments/Teach3/03Teach.php
@@ -1,3 +1,9 @@
+<?php
+$continents = $_POST['continents'];
+
+?>
+
+
 <html>
 <body>
 
@@ -5,6 +11,21 @@ Welcome <?php echo htmlspecialchars($_POST["name"]); ?><br>
 mailto: <?php echo htmlspecialchars($_POST["email"]);?><br><br>
 Major: <?php echo htmlspecialchars($_POST["major"]);?><br>
 comments:<?php echo htmlspecialchars($_POST["comment"]);?><br>
+<br>
+<br>
+<h3>Part 3 - continents the user has visited</h3> <br> <br>
+
+
+
+
+
+Continents:<?php
+foreach ($continents as $val){
+    echo $val."<br>";
+}
+?>
+
+
 
 </body>
 </html>
