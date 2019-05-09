1ea11b647db70c1012bfbe6b0a0660af17f91e41
Isaac Sanchez <san16044@byui.edu>
2019-05-09 01:23:22 -0500

    DB prove03 testing

--- web/week03/03Prove.php
+++ web/week03/03Prove.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="utf-8" />
     <title>Browse Items</title>
-    <link href="styleSheet.css" rel="stylesheet" />
+    <link href="03ProveStyle.css.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
