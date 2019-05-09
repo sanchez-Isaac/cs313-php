e9900170d22a2a1679f41f320d1f3b94e5bfeb4c
Isaac Sanchez <san16044@byui.edu>
2019-05-09 18:12:45 -0500

    DB prove03 testing

--- web/week03/viewcart.php
+++ web/week03/viewcart.php
@@ -143,9 +143,9 @@ function pre_r($array)
             foreach ($_SESSION['shopping_cart'] as $key => $product):
              ?>
             <tr>
-                <td><?php echo $product['name']; ?>/td>
-                <td><?php echo $product['quantity']; ?>/td>
-                <td>$ <?php echo $product['price']; ?>/td>
+                <td><?php echo $product['name']; ?></td>
+                <td><?php echo $product['quantity']; ?></td>
+                <td>$ <?php echo $product['price']; ?></td>
                 <td>$ <?php echo number_format($product['quantity'] * $product['price'], 2); ?> </td>
                 <td><a href="viewcart.php?action=delete&id<?php echo $product['id']; ?> ">
                         <div class="btn-danger"> Remove</div>
