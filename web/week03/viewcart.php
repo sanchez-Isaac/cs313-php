d748d321866cf6459d22c2dc2f6f07c961e79f78
Isaac Sanchez <san16044@byui.edu>
2019-05-09 18:54:33 -0500

    DB prove03 testing

--- web/week03/viewcart.php
+++ web/week03/viewcart.php
@@ -37,7 +37,7 @@ if(filter_input(INPUT_POST, 'add_to_cart')){
 }
 
 if(filter_input(INPUT_GET, 'action')== 'delete') {
-    foreach ($_SESSION['shopping_cart'] as $key => $product) {
+    foreach($_SESSION['shopping_cart'] as $key => $product) {
         if ($product['id'] == filter_input(INPUT_GET, 'id')) {
             unset($_SESSION['shopping_cart'][$key]);
         }
@@ -46,7 +46,7 @@ if(filter_input(INPUT_GET, 'action')== 'delete') {
 }
 
 
-//pre_r($_SESSION);
+pre_r($_SESSION);
 
 function pre_r($array)
 {
