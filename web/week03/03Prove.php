b052d752c1eec114d18056a368c4a579d449b6c9
Isaac Sanchez <san16044@byui.edu>
2019-05-09 00:41:28 -0500

    DB prove03 testing

--- web/week03/03Prove.php
+++ web/week03/03Prove.php
@@ -1,18 +1,18 @@
 <?php
 
-$con = "dbname=dbe43cu3qv6rjv port=5432 user=qvuhdtpvtfgheg password=e50137efad45ae63f6a5fa81a0f202027a32756361cc2b8c818d5acecc268e08 sslmode=require";
-if (!$con)
-{
-    echo "Database connection failed.";
-}
-else
-{
-    echo "Database connection success.";
-}
+$host = "ec2-23-21-129-125.compute-1.amazonaws.com";
+$user = "qvuhdtpvtfgheg";
+$pass = "e50137efad45ae63f6a5fa81a0f202027a32756361cc2b8c818d5acecc268e08*";
+$db = "dbe43cu3qv6rjv";
+$port = "5432";
+
+$con = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass")
+or die ("Could not connect to server\n");
 
 
 $query = 'SELECT * FROM products ORDER by id ASC';
-$result = pg_query($con, $query);
+
+$result = pg_query( $con, $query);
 
 if($result){
     if(pg_num_rows($result)>0){
@@ -21,33 +21,5 @@ if($result){
         }
     }
 }
-
+pg_close($con);
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title>Browse Items</title>
-    <link href="styleSheet.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-
-</head>
-<body>
-
-<h1>Available Items</h1><br><br>
-
-<?php
-
-
-?>
-
-
-
-</body>
-</html>
