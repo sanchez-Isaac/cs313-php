f598db6939f657334d66ff508cea502b94432b4a
Isaac Sanchez <san16044@byui.edu>
2019-05-09 17:55:27 -0500

    DB prove03 testing

--- web/week03/viewcart.php
+++ web/week03/viewcart.php
@@ -110,24 +110,57 @@ function pre_r($array)
     endif;
 
     ?>
+<div style="clear:both"></div>
+    <br/>
+    <div class="table-responsive">
+        <table class="table">
+            <tr><th colspan="5"><h3>Order Details</h3></th> </tr>
+            <tr>
+                <th width="40%">Product Name</th>
+                <th width="10%">Quantity</th>
+                <th width="20%">Price</th>
+                <th width="15%">Total</th>
+                <th width="5%">Action</th>
+            </tr>
+            <?php
+            if(!empty($_SESSION['shopping_cart'])):
 
-
-</div>
-<tr>
-    <td colspan="5">
-        <?php
-        if(isset($_SESSION['shopping_cart'])):
-            if (count($_SESSION['shopping_cart'])>0):
-                ?>
-                <a href="viewcart.php" class="button" > View Cart </a>
-            <?php endif; endif;?>
-    </td>
-
-</tr>
-
-
+            $total = 0;
+            foreach ($_SESSION['Shopping_cart'] as $key => $product):
+             ?>
+            <tr>
+                <td><?php echo $product['name']; ?>/td>
+                <td><?php echo $product['quantity']; ?>/td>
+                <td><?php echo $product['price']; ?>/td>
+                <td><?php echo number_format($product['quantity'] * $product['price'], 2); ?> </td>
+                <td><a href="viewcart.php?action=delete&id<?php echo $product['id']; ?> ">
+                        <div class="button-danger"> Remove</div>
+                    </a>
+                </td>
+            </tr>
+            <?php
+            $total = $total + ($product['quantity'] * $product['price']);
+            endforeach;
+            ?>
+            <tr>
+                <td colspan="3" align="right"> Total</td>
+                <td align="right">$ <?php echo number_format($total, 2); ?></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td colspan="5">
+                    <?php
+                    if (isset($_SESSION['shopping_cart'])):
+                    if (count($_SESSION['shopping_cart'])> 0):
+                    ?>
+                    <a href="#" class="button">Checkout</a>
+                    <?php endif; endif; ?>
+                </td>
+            </tr>
+            <?php
+            endif;
+            ?>
+        </table>
+    </div>
 </body>
 </html>
-
-
-
