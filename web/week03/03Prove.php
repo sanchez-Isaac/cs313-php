e3eb4580023aff3b5674b4ee49b2767c5284d513
Isaac Sanchez <san16044@byui.edu>
2019-05-09 00:44:02 -0500

    DB prove03 testing

--- web/week03/03Prove.php
+++ web/week03/03Prove.php
@@ -2,7 +2,7 @@
 
 $host = "ec2-23-21-129-125.compute-1.amazonaws.com";
 $user = "qvuhdtpvtfgheg";
-$pass = "e50137efad45ae63f6a5fa81a0f202027a32756361cc2b8c818d5acecc268e08*";
+$pass = "e50137efad45ae63f6a5fa81a0f202027a32756361cc2b8c818d5acecc268e08";
 $db = "dbe43cu3qv6rjv";
 $port = "5432";
 
