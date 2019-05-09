cb595c5a57cd65a73d5fe78562b699f1e422a57c
Isaac Sanchez <san16044@byui.edu>
2019-05-09 16:38:42 -0500

    DB prove03 testing

--- web/week03/03Prove.php
+++ web/week03/03Prove.php
@@ -98,8 +98,8 @@ if($result):
                     <h4 class="text-info"><?php echo $product['name']; ?></h4>
                     <h4>$ <?php echo $product['price']; ?></h4>
                     <input type="text" name="quantity" class="form-control" value="1" />
-                    <input type="hidden" name="name" value="<?php echo product['name']; ?>" />
-                    <input type="hidden" name="price" value="<?php echo product['price']; ?>" />
+                    <input type="hidden" name="name" value="<?php echo $product['name']; ?>" />
+                    <input type="hidden" name="price" value="<?php echo $product['price']; ?>" />
                <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-info" Value="Add to Cart" />
                 </div>
             </form>
