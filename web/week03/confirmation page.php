5039c2d513c73e2a6bb1221ce2f89c864dfabce9
Isaac Sanchez <san16044@byui.edu>
2019-05-10 16:29:18 -0500

    DB prove03 testing

--- web/week03/confirmation page.php
+++ web/week03/confirmation page.php
@@ -1,10 +1,61 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: idsm_
- * Date: 08/05/2019
- * Time: 06:33 PM
- */
+<?PHP
+session_start();
+$product_ids = array();
+//session_destroy();
+if(filter_input(INPUT_POST, 'add_to_cart')){
+    if(isset ($_SESSION['shopping_cart'])){
+        $count = count($_SESSION['shopping_cart']);
+
+        $product_ids = array_column($_SESSION['shopping_cart'], 'id');
+
+        if (!in_array(filter_input(INPUT_GET,'id'), $product_ids)){
+            $_SESSION['shopping_cart'][$count] = array
+            (
+                'id'=>filter_input(INPUT_GET,'id'),
+                'name' => filter_input(INPUT_POST, 'name'),
+                'price' => filter_input(INPUT_POST, 'price'),
+                'quantity' => filter_input(INPUT_POST, 'quantity')
+            );
+        }
+        else {
+            for ($i = 0; $i < count($product_ids); $i++){
+                if ($product_ids[$i] == filter_input(INPUT_GET, 'id')){
+                    $_SESSION['shopping_cart'][$i]['quantity'] += filter_input(INPUT_POST,'quantity');
+                }
+            }
+        }
+    }
+    else{
+        $_SESSION['shopping_cart'][0] = array
+        ('id'=>filter_input(INPUT_GET,'id'),
+            'name' => filter_input(INPUT_POST, 'name'),
+            'price' => filter_input(INPUT_POST, 'price'),
+            'quantity' => filter_input(INPUT_POST, 'quantity')
+
+        );
+    }
+}
+
+if(filter_input(INPUT_GET, 'action')== 'delete') {
+    foreach($_SESSION['shopping_cart'] as $key => $product) {
+        if ($product['id'] == filter_input(INPUT_GET, 'id')) {
+            unset($_SESSION['shopping_cart'][$key]);
+        }
+    }
+    $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
+}
+
+
+//pre_r($_SESSION);
+
+function pre_r($array)
+{
+    echo '<pre>';
+    print_r($array);
+    echo '</pre>';
+}
+
+
 ?>
 
 <!DOCTYPE html>
