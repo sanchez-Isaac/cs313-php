42636a3e7894958d22ab07964425e98a13b7e0f6
Isaac Sanchez <san16044@byui.edu>
2019-05-10 19:37:51 -0500

    DB prove03 testing

--- web/week03/confirmation page.php
+++ web/week03/confirmation page.php
@@ -56,7 +56,7 @@ function pre_r($array)
 <div>
 
         <h1 class="mb-3">Billing address</h1><br>
-        <form class="needs-validation" novalidate="" method="post" action="confirmation%20page.php?action=add&id=<?php echo $_SESSION['firstName']; ?>">
+        <form class="needs-validation" novalidate="" method="post" action="thankyou.php?action=add&id=<?php echo $_SESSION['firstName']; ?>">
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="firstName">First name</label>
@@ -135,7 +135,7 @@ function pre_r($array)
                 </div>
 
 
-                <div class="col-md-3 mb-3"> 
+                <div class="col-md-3 mb-3">
                     <label for="zip">Zip</label>
                     <input type="text" class="form-control" id="zip" placeholder=""  name="zip"required="">
                     <div class="invalid-feedback">
