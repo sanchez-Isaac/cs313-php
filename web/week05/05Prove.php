7865e5b09723ac686cf252b7c25620cf8f0a5c95
Isaac Sanchez <san16044@byui.edu>
2019-05-21 20:26:49 -0500

    Prove05 first pushes

--- web/week05/05Prove.php
+++ web/week05/05Prove.php
@@ -69,7 +69,7 @@ $result2 = pg_query( $con, $sqlQuery);
 
 if (pg_num_rows($result2)>0){
     while($row=pg_fetch_array($result2)){
-        echo "TEST1 ".$row[1]. "TEST2 ".$row[2]. "TEST3 ".$row[3]. "TEST4 ".$row[4]. "TEST5 ".$row[5]. "TEST6 ". $row[6];
+        echo "TEST id ".$row[0]. " TEST Barcode ".$row[1]. " TEST Name ".$row[2]. " TEST type ".$row[3]. " TEST price ".$row[4]. "TEST quantity ". $row[5];
         echo"<br>";
     }
 }
