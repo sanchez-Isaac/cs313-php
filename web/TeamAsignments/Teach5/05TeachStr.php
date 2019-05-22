2f56d08dd6254272b4f44efc09ac5ce74372086d
Isaac Sanchez <san16044@byui.edu>
2019-05-22 17:53:24 -0500

    First push of team05

--- web/TeamAsignments/Teach5/05TeachStr.php
+++ web/TeamAsignments/Teach5/05TeachStr.php
@@ -1,66 +1,61 @@
 <?php
-
-
+​
+​
 $host = "ec2-23-21-129-125.compute-1.amazonaws.com";
 $user = "qvuhdtpvtfgheg";
 $pass = "e50137efad45ae63f6a5fa81a0f202027a32756361cc2b8c818d5acecc268e08";
 $db = "dbe43cu3qv6rjv";
 $port = "5432";
-
+​
 $con = pg_connect("host=$host port=$port dbname=$db user=$user password=$pass")
 or die ("Could not connect to server\n");
-
-
+​
+$bookName = $_POST['Book'];
 $query = 'SELECT * FROM Scriptures WHERE book ='. $bookName;
 $result = pg_query( $con, $query);
-
-
+​
+​
 ?>
-
-
+​
+​
 <!doctype html>
-
+​
 <html lang="en">
 <head>
     <meta charset="utf-8">
-
+    ​
     <title>The HTML5 Herald</title>
     <meta name="description" content="Teach 05">
     <meta name="author" content="SitePoint">
-
+    ​
     <link rel="stylesheet" href="css/styles.css?v=1.0">
-
+    ​
 </head>
-
+​
 <body>
 <script src="js/scripts.js"></script>
-
-
+​
 <form action = "05Teach.php" method = "POST">
-<select name = "Book">
-    <option value = "John">John</option>
-    <option value = "Doctrine and Covenants">D&C</option>
-    <option value = "Mosiah">Mosiah</option>
-    <input type = "submit" value = "Search">
-</select>
+    <select name = "Book">
+        <option value = "John">John</option>
+        <option value = "Doctrine and Covenants">D&C</option>
+        <option value = "Mosiah">Mosiah</option>
+        <input type = "submit" value = "Search">
+    </select>
 </form>
-
-
+​
 <?php
 if (pg_num_rows($result) > 0) {
     // output data of each row
     while($row = pg_fetch_assoc($result)) {
-        echo "<b>".$row["book"]. "<b></b>" . " - " . $row["chapter"]. ":" . $row["verse"]. "<br>" . $row["content"] . "<br><br>";
+        echo $row["book"]. " " . $row["chapter"]. ":" . $row["verse"]. "<br>" . $row["content"] . "<br><br>";
     }
 } else {
-
+    ​
 }
-
-
+​
+$conn->close();
+​
 ?>
 </body>
-
-<?php
-$conn->close();
-?>
 </html>
