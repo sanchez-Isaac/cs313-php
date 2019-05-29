3c5605fc3c9d06fb1c48be81193c640a64f4c19a
Isaac Sanchez <san16044@byui.edu>
2019-05-29 00:36:41 -0500

    Prove06 testing

--- web/week06/insert_query.php
+++ web/week06/insert_query.php
@@ -1,11 +1,11 @@
 <?php
 include_once 'DbConnect.php.php';
 
-$item_name = pg_escape_string($_POST['item_name']);
-$item_type = pg_escape_string($_POST['item_type']);
-$item_price = pg_escape_string($_POST['item_price']);
-$item_quantity = pg_escape_string($_POST['item_quantity']);
-$photo_desc = pg_escape_string($_POST['photo_desc']);
+$item_name = $_POST['item_name'];
+$item_type = $_POST['item_type'];
+$item_price = $_POST['item_price'];
+$item_quantity = $_POST['item_quantity'];
+$photo_desc = $_POST['photo_desc'];
 
 
 $sql = "INSERT INTO items(item_name, item_type, item_price, item_quantity)
@@ -13,4 +13,4 @@ VALUES($item_name, $item_type, $item_price,$item_quantity, $photo_desc);";
 
 pg_query($con ,$sql);
 
-header("Location: insert_items?insert=success");
+header("Location: insert_items.php?insert=success");
