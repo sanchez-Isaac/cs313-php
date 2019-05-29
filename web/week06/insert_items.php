2e7fe010d5e65c6ea75987f2d2d16cafdbced7ba
Isaac Sanchez <san16044@byui.edu>
2019-05-28 23:50:19 -0500

    Prove06 testing

--- web/week06/insert_items.php
+++ web/week06/insert_items.php
@@ -4,7 +4,7 @@ require ('DbConnect.php');
 $con = get_db();
 
 
-if(isset($_POST['insert'])) {
+
 
     $item_name = pg_escape_string($_POST['item_name']);
     $item_type = pg_escape_string($_POST['item_type']);
@@ -21,8 +21,8 @@ VALUES(DEFAULT, DEFAULT, :$item_name, :$item_type, :$item_price,:$item_quantity,
     $query->bindValue(':$photo_desc', $photo_desc, PDO::PARAM_STR);
     $query->execute();
 
-    $result = pg_query($con, $query);
-}
+
+
 
 
 
