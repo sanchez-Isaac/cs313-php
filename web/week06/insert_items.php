2b588d210ce0d1eb9cdf49198e4d48cdf20feebd
Isaac Sanchez <san16044@byui.edu>
2019-05-29 00:14:10 -0500

    Prove06 testing

--- web/week06/insert_items.php
+++ web/week06/insert_items.php
@@ -24,8 +24,8 @@ if(isset($_POST['insert'])) {
     */
 
 
-    $sql = "INSERT INTO items(item_id, item_barcode, item_name, item_type, item_price, item_quantity)
-VALUES(DEFAULT, DEFAULT, $item_name, $item_type, $item_price,$item_quantity, $photo_desc)";
+    $sql = "INSERT INTO items(item_name, item_type, item_price, item_quantity)
+VALUES($item_name, $item_type, $item_price,$item_quantity, $photo_desc)";
 
     $result = pg_query($con ,$sql);
 }
