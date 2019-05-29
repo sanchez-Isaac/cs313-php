1359da4263c1ab43ae40333b81d05212a937d6e6
Isaac Sanchez <san16044@byui.edu>
2019-05-29 18:51:43 -0500

    First push of teach6

--- web/TeamAsignments/Teach6/query_scriptures.php
+++ web/TeamAsignments/Teach6/query_scriptures.php
@@ -6,7 +6,7 @@ $con = get_db();
 
 
 
-$query = 'SELECT * FROM Scriptures';
+$query = 'SELECT * FROM scriptures s, scripture_and_topic st,topic t WHERE s.id = st.scrip_id and st.topic_Id=t.id;';
 $result = pg_query( $con, $query);
 
 
@@ -41,15 +41,7 @@ $result2 = pg_query( $con, $query2);
 if (pg_num_rows($result) > 0) {
     // output data of each row
     while($row = pg_fetch_assoc($result)) {
-        echo "<br><b>".$row["book"]. "</b>" . " - " . $row["chapter"]. ":" . $row["verse"]. "<br>" . $row["content"] . "<br>";
-
-
-           // output data of each row
-           while ($row = pg_fetch_assoc($result2)) {
-               echo "<b> Topic: " . $row["name"] . "<br>";
-           }
-
-
+        echo "<br><b>".$row["book"]. "</b>" . " - " . $row["chapter"]. ":" . $row["verse"]. "<br>" . $row["content"] . "<br>" . "<b> Topic: " . $row["name"] . "<br>";
     }
 
 }
