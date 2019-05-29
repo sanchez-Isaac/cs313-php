3caecad8cbdaa5f4326eaf7649eabe60ecea5152
Isaac Sanchez <san16044@byui.edu>
2019-05-29 01:01:33 -0500

    Prove06 testing

--- web/week06/insert_query.php
+++ web/week06/insert_query.php
@@ -6,10 +6,23 @@ $item_type = $_POST['item_type'];
 $item_price = $_POST['item_price'];
 $item_quantity = $_POST['item_quantity'];
 $photo_desc = $_POST['photo_desc'];
+$item_id;
+
+$query = 'SELECT item_id FROM items';
+$result = pg_query( $con, $query);
 
 
-$sql = "INSERT INTO items(item_name, item_type, item_price, item_quantity, photo_desc)
-VALUES($item_name, $item_type, $item_price,$item_quantity, $photo_desc);";
+while($rows = pg_fetch_assoc($result))
+{
+    $item_id =  $rows['item_id'];
+}
+
+
+
+
+
+$sql = "INSERT INTO items(item_id, item_barcode, item_name, item_type, item_price, item_quantity, photo_desc)
+VALUES($item_id, $item_id, $item_name, $item_type, $item_price,$item_quantity, $photo_desc);";
 
 pg_query($con ,$sql);
 
