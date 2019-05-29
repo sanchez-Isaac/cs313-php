c14e88e08d9df87741a746d02fad1d977195524a
Isaac Sanchez <san16044@byui.edu>
2019-05-29 17:11:00 -0500

    First push of teach6

--- web/TeamAsignments/Teach6/insert.php
+++ web/TeamAsignments/Teach6/insert.php
@@ -7,9 +7,11 @@ $book = pg_escape_string($_POST['book']);
 $chapter = pg_escape_string($_POST['chapter']);
 $verse = pg_escape_string($_POST['verse']);
 $content = pg_escape_string($_POST['content']);
-$topic_charity = pg_escape_string($_POST['Charity']);
-$topic_faith = pg_escape_string($_POST['Faith']);
-$topic_sacrifice = pg_escape_string($_POST['Sacrifice']);
+
+
+$topic_charity = pg_escape_string($_POST['3']);
+$topic_faith = pg_escape_string($_POST['1']);
+$topic_sacrifice = pg_escape_string($_POST['2']);
 
 $item_id = 1 ;
 
@@ -31,9 +33,17 @@ pg_query($con ,$sql);
 
 
 
+$sql2 = "INSERT INTO scripture_and_topic(srip_id, topic_id)
+             VALUES($item_id, $topic_charity;";
+pg_query($con, $sql2);
+
 $sql3 = "INSERT INTO scripture_and_topic(srip_id, topic_id)
-             VALUES($item_id, $item_id;";
+             VALUES($item_id, $topic_faith;";
 pg_query($con, $sql3);
+
+$sql4 = "INSERT INTO scripture_and_topic(srip_id, topic_id)
+             VALUES($item_id, $topic_sacrifice;";
+pg_query($con, $sql4);
 
 
 pg_close($con);
