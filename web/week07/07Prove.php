b6120e37191e90f12ef4fd2439e546812d92f058
Isaac Sanchez <san16044@byui.edu>
2019-06-04 15:33:23 -0500

    Week07 Project delivery update

--- web/week07/07Prove.php
+++ web/week07/07Prove.php
@@ -82,7 +82,8 @@ if(isset($_POST['login_btn'])){
                 <input type="submit"  name="login_btn" value="Login" class="btn btn-info btn-block">
                 </form>
             </div>
-            <small class="labelor">Or</small>
+
+            <div class="labelor">Or</div>
             <input type="button" class="btn btn-success btn-block" value="Create Account">
                 <a href="store.php">
                 </a>
