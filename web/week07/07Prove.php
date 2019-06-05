afa09acf1db3e004acaa8d958027671f73029a3c
Isaac Sanchez <san16044@byui.edu>
2019-06-05 02:03:11 -0500

    Week07 Project delivery update

--- web/week07/07Prove.php
+++ web/week07/07Prove.php
@@ -133,6 +133,7 @@ WHERE cu.customer_id = ad.address_id and cu.customer_id = id.login_id and id.ema
                 <input type="submit"  name="login_btn" value="Login" class="btn btn-info btn-block">
                     <div class="labelor"><b>Or</b></div>
                     <input type="button" class="btn btn-success btn-block" formaction="create_account.php" value="Create Account">
+                    <button formaction="create_account.php" class="w3-button w3-green w3-padding-large">Create Account</button>
 
             </div>
                 </form>
