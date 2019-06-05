22a4a3ba4828a82e57ce78e54bef20e9df937b03
Isaac Sanchez <san16044@byui.edu>
2019-06-04 19:09:08 -0500

    Week07 Project delivery update

--- web/week07/itemsSearch.php
+++ web/week07/itemsSearch.php
@@ -132,6 +132,30 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
 <br>
 <br>
 <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 <div class="footer">
     <p>CS 313 - Web Engineering II    &copy; 2016 - <?php echo date("Y");?> </p>
 
