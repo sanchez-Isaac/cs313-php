01ff935f00e7444d80d388d77a6c11552e679aef
Isaac Sanchez <san16044@byui.edu>
2019-06-05 01:07:09 -0500

    Week07 Project delivery update

--- web/week07/confirmation.php
+++ web/week07/confirmation.php
@@ -3,15 +3,21 @@ session_start();
 
 
 if (isset($_POST['Submit'])) {
-    $_SESSION['firstName'] = test_input($_POST['firstName']);
-    $_SESSION['lastName'] = test_input($_POST['lastName']);
+    $_SESSION['first_name'] = test_input($_POST['first_name']);
+    $_SESSION['middle_name'] = test_input($_POST['middle_name']);
+    $_SESSION['last_name'] = test_input($_POST['last_name']);
     $_SESSION['email'] = test_input($_POST['email']);
-    $_SESSION['address'] = test_input($_POST['address']);
-    $_SESSION['address2'] = test_input($_POST['address2']);
+    $_SESSION['street'] = test_input($_POST['street']);
+    $_SESSION['ext_home_number'] = test_input($_POST['ext_home_number']);
+    $_SESSION['city']  = test_input($_POST['city']);
     $_SESSION['country'] = test_input($_POST['country']);
     $_SESSION['state'] = test_input($_POST['state']);
     $_SESSION['zip'] = test_input($_POST['zip']);
+    $_SESSION['telephone'] = test_input($_POST['telephone']);
+
     header("Location:thankyou.php");
+
+
 }
 function test_input($data)
 {
@@ -60,23 +66,38 @@ function pre_r($array)
     <br/>
     <div>
 
+
+
+
+
+
+
+
+
+
         <h1 class="mb-3">Billing address</h1><br>
         <form class="needs-validation" novalidate="" method="post" action="">
             <div class="row">
                 <div class="col-md-6 mb-3">
                     <label for="firstName">First name</label>
-                    <input type="text" class="form-control" name="firstName" id="firstName" placeholder="" value="" required>
+                    <input type="text" class="form-control" name="first_name" id="first_name" placeholder="" value="" required>
                     <div class="invalid-feedback">
-                        Valid first name is required.
+                    </div>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                    <label for="firstName">Middle Name</label>
+                    <input type="text" class="form-control" name="middle_name" id="middle_name" placeholder="" value="" >
+                    <div class="invalid-feedback">
                     </div>
                 </div>
 
 
                 <div class="col-md-6 mb-3">
                     <label for="lastName">Last name</label>
-                    <input type="text" class="form-control" id="lastName" name="lastName" placeholder="" value="" required>
+                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="" value="" required>
                     <div class="invalid-feedback">
-                        Valid last name is required.
+
                     </div>
                 </div>
             </div>
@@ -84,26 +105,34 @@ function pre_r($array)
 
 
             <div class="mb-3">
-                <label for="email">Email <span class="text-muted">(Optional)</span></label>
+                <label for="email">Email </label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="you@example.com">
                 <div class="invalid-feedback">
-                    Please enter a valid email address for shipping updates.
+
                 </div>
             </div>
-            <br>
 
             <div class="mb-3">
-                <label for="address">Address</label>
-                <input type="text" class="form-control" id="address" name="address" placeholder="1234 Main St" required>
+                <label for="telephone">Email </label>
+                <input type="tel" class="form-control" id="telephone" name="telephone" aria-describedby="emailHelp" placeholder="you@example.com">
                 <div class="invalid-feedback">
-                    Please enter your shipping address.
+
                 </div>
             </div>
             <br>
 
             <div class="mb-3">
-                <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-                <input type="text" class="form-control" id="address2" name="address2" placeholder="Apartment or suite">
+                <label for="street">Street</label>
+                <input type="text" class="form-control" id="street" name="street" placeholder="1234 Main St" required>
+                <div class="invalid-feedback">
+
+                </div>
+            </div>
+            <br>
+
+            <div class="mb-3">
+                <label for="address2">Ext.#</label>
+                <input type="text" class="form-control" id="ext_home_number" name="ext_home_number" placeholder="567">
             </div>
             <br>
 
@@ -113,38 +142,38 @@ function pre_r($array)
                     <select class="custom-select d-block w-100" id="country" name="country" required>
                         <option value="">Choose...</option>
                         <option>United States</option>
-
-
+                        <option>United States</option>
+                        <option>United States</option>
+                        <option>United States</option>
+                        <option>United States</option>
+                        <option>United States</option>
+                        <option>United States</option>
                     </select>
-                    <div class="invalid-feedback">
-                        Please select a valid country.
-                    </div>
                 </div>
 
 
-                <div class="col-md-4 mb-3">
-                    <label for="state">State</label>
-                    <select class="custom-select d-block w-100" id="state" name="state" required>
-                        <option value="">Choose...</option>
-                        <option>CA</option>
-                        <option>MA</option>
-                        <option>NH</option>
-                        <option>NC</option>
-                        <option>SC</option>
-                        <option>NY</option>
 
-                    </select>
+                <div class="mb-3">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city" name="city" placeholder="Los Angeles" required>
                     <div class="invalid-feedback">
-                        Please provide a valid state.
                     </div>
+
+                    <div class="mb-3">
+                        <label for="state">State</label>
+                        <input type="text" class="form-control" id="city" name="state" placeholder="California" required>
+                        <div class="invalid-feedback">
+                        </div>
+                    </div>
+
                 </div>
 
 
                 <div class="col-md-3 mb-3">
                     <label for="zip">Zip</label>
-                    <input type="text" class="form-control" id="zip" placeholder=""  name="zip"required>
+                    <input type="text" class="form-control" id="zip" placeholder=""  name="zip" required>
                     <div class="invalid-feedback">
-                        Zip code required.
+
                     </div>
                 </div>
             </div>
