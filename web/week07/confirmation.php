60f1d9891a1dd06fe1974fd31e4b992cdd6417a2
Isaac Sanchez <san16044@byui.edu>
2019-06-05 01:18:31 -0500

    Week07 Project delivery update

--- web/week07/confirmation.php
+++ web/week07/confirmation.php
@@ -165,11 +165,10 @@ function pre_r($array)
                         </div>
                     </div>
 
-                <div class="col-md-3 mb-3">
+                <div class="col-md-5 mb-3">
                     <label for="zip">Zip</label>
-                    <input type="text" class="form-control" id="zip" placeholder=""  name="zip"required="">
+                    <input type="text" class="form-control" id="zip" placeholder=""  name="zip" required>
                     <div class="invalid-feedback">
-                        Zip code required.
                     </div>
                 </div>
             </div>
