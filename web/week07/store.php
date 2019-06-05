bf813a88954488082f0256363ad99aad777f5f6d
Isaac Sanchez <san16044@byui.edu>
2019-06-04 23:43:59 -0500

    Week07 Project delivery update

--- web/week07/store.php
+++ web/week07/store.php
@@ -53,6 +53,7 @@ function pre_r($array)
     <meta charset="utf-8" />
     <title>Browse Items</title>
     <link href="03ProveStyle.css" rel="stylesheet" />
+    <link href="homestyle.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -89,7 +90,6 @@ function pre_r($array)
 
 
 
-            <button type="submit">Login</button>
 
 
 <h1>Available Items</h1><br><br>
