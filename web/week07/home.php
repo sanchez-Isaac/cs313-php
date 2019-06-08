dbd5bc6e4979a71b039428ecb21cf51b58602c05
Isaac Sanchez <san16044@byui.edu>
2019-06-07 21:07:17 -0500

    New updates to Prove7 final delivery

--- web/week07/home.php
+++ web/week07/home.php
@@ -111,20 +111,27 @@ if(!isset($_SESSION['username']))
 <br>
 <br>
 <br>
-<button type=button" data-target="#createUserModal" data-Toggle="modal" class="w3-button w3-green w3-padding-large">Add Users modal</button>
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+</button>
 
-<div class="modal fade" id="createUserModal" tabindex="-1">
-    <div class="modal-dialog">
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h3> This action will log you out of your account</h3>
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
-                <p>Click on Continue if you agree with this</p>
+                ...
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel;</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">Continue;</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
