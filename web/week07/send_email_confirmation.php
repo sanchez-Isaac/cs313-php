564a09412f5edace556221adcb447e55088cd207
Isaac Sanchez <san16044@byui.edu>
2019-06-08 18:44:44 -0500

    New updates to Prove7 on sending email confirmation

--- web/week07/send_email_confirmation.php
+++ web/week07/send_email_confirmation.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: idsm_
- * Date: 08/06/2019
- * Time: 02:27 PM
- */
+require_once ('PHPMailer/PHPMailerAutoload.php');
+include_once 'DbConnect.php';
+
+$con = get_db();
+
+$first_name = $_SESSION['first_name'];
+$middle_name = $_SESSION['middle_name'];
+$last_name = $_SESSION['last_name'];
+$street = $_SESSION['street'];
+$ext_home_number = $_SESSION['ext_home_number'];
+$city = $_SESSION['city'];
+$country = $_SESSION['country'];
+$state = $_SESSION['state'];
+$zip = $_SESSION['zip'];
+$telephone = $_SESSION['telephone'];
+$customer_id = $_SESSION['customer_id'];
+
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth(true);
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+$mail->isHTML();
+$mail->Username = 'isaac.sanchez.mbm@gmail.com';
+$mail->Password = 'stjimmy1';
+$mail->SetFrom('no-reply@cs313.byui.edu');
+$mail->Subject = 'Testing Hello world';
+$mail->Body = 'Testing body Hello world';
+$mail->AddAddress('idsm_2000@hotmail.com');
+$mail->Send();
+
+
+
