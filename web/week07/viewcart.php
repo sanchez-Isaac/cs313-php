78557b40d18b193a79f8846bab1b2217c072177b
Isaac Sanchez <san16044@byui.edu>
2019-06-08 21:44:37 -0500

    New updates to Prove7 on order history

--- web/week07/viewcart.php
+++ web/week07/viewcart.php
@@ -22,7 +22,7 @@ if(filter_input(INPUT_GET, 'action')== 'delete') {
     <meta charset="utf-8" />
     <title>Browse Items</title>
     <link href="03ProveStyle.css" rel="stylesheet" />
-    <link href="homestyle.css" rel="stylesheet" />
+    <link href="ProveStyle.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
